fa6278a12ac003f69aef500f4796c2fe0e85279d
hjin <hjin@megastudy.net>
2024-07-03 18:09:00 +0900

    vue 프로젝트 시작

---
+++ hjin-web/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from '@/App.vue'
+
+import '@/assets/scss/layout.scss';
+import '@/assets/scss/base.scss';
+import '@/assets/scss/common.scss'
+createApp(App).mount('#app')
